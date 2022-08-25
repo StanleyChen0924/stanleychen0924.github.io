26151ae24d8c9dbe5de3da4c46b602e798486860
Stanley Chen <86638972+StanleyChen0924@users.noreply.github.com>
2022-08-25 15:07:07 +0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,32 +1,15 @@
 <?php
-/*
- * This file is part of the Shieldon Simple Cache package.
- *
- * (c) Terry L. <contact@terryl.in>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 declare(strict_types=1);
 
-/**
- * Register to PSR-4 autoloader.
- *
- * @return void
- */
+
 function simplecache_register()
 {
     spl_autoload_register('simplecache_autoload', true, false);
 }
 
-/**
- * PSR-4 autoloader.
- *
- * @param string $className
- * 
- * @return void
- */
+
 function simplecache_autoload($className)
 {
     $prefix = 'Shieldon\\SimpleCache\\';
