0b9a2490378a59583066b70081211b9ad63a0d5c
Stanley Chen <86638972+StanleyChen0924@users.noreply.github.com>
2022-09-29 15:26:15 +0800

    Add files via upload

---
+++ video/left1.js
@@ -0,0 +1,13 @@
+﻿function showzk(){
+        menubox.style.display = "block";
+        zk.style.display = "none";
+        sh.style.display = "block";
+        playbox.style.left="202px";
+     }
+
+function showsh(){
+        menubox.style.display = "none";
+        zk.style.display = "block";
+        sh.style.display = "none";
+        playbox.style.left="7px";
+     }
---
+++ video/tj.js
@@ -0,0 +1,45 @@
+﻿//百度统计lu1.cc
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a29f1757d79597b75459f052968513a2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//百度统计3666.co
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c5c0c780911a53671e30a93cde17d384";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//百度统计87881.cn
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?66740b7009dca8080ce63f9da7d29a26";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//新的51啦lu1.cc
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JJMGAjCrIEu114oq\",ck:\"JJMGAjCrIEu114oq\"});</script>")
+
+//新的51啦87881.cn
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JNwRVdtpzDA0HU4u\",ck:\"JNwRVdtpzDA0HU4u\"});</script>")
+
+//老的51啦
+document.write ('<script type="text/javascript"  src="http://www.lu1.cc/a/js/lu1v5.js"></script>');
+
+//灵雀监控
+  !(function(c,i,e,b){
+  var h=i.createElement("script");
+  var f=i.getElementsByTagName("script")[0];
+  h.type="text/javascript";
+  h.crossorigin=true;
+  h.onload=function(){new c[b]["Monitor"]().init({id:"JWH9ppzjvmG8Jfvf"});};
+  f.parentNode.insertBefore(h,f);h.src=e;})(window,document,"https://sdk.51.la/perf/js-sdk-perf.min.js","LingQue");
+  
